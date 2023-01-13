f8292b29be885b74daca6f2bf6a36679c42a121d
chamberis <karolisjakstas@gmail.com>
2023-01-13 14:22:42 +0200

    proper cssselectors and method names

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,7 +1,5 @@
+import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.lang.module.Configuration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -22,7 +20,7 @@ public class FirstTest {
 //    }
 
     @Test
-    public void CrudTodo() {
+    public void crudTodo() {
         // open TodoMVC page https://todomvc.com/examples/emberjs/
         open("https://todomvc.com/examples/emberjs/");
         // add todos: "a", "b", "c"
@@ -30,18 +28,19 @@ public class FirstTest {
         $("#new-todo").setValue("b").pressEnter();
         $("#new-todo").setValue("c").pressEnter();
         // todos should be "a", "b", "c"
-        $("li:nth-child(1)").shouldHave(exactText("a"));
-        $("li:nth-child(2)").shouldHave(exactText("b"));
-        $("li:nth-child(3)").shouldHave(exactText("c"));
+//        $("li:nth-child(1)").shouldHave(exactText("a"));
+//        $("li:nth-child(2)").shouldHave(exactText("b"));
+//        $("li:nth-child(3)").shouldHave(exactText("c"));
+        $$("#todo-list li").shouldHave(CollectionCondition.texts("a", "b", "c"));
         // toggle b
-        $("li:nth-child(2) .toggle").click();
+        $("#todo-list li:nth-child(2) .toggle").click();
         // completed todos should be b
-        $("li:nth-child(3) a").click();
+        $("#todo-list li:nth-child(3) a").click();
         $(".view").shouldHave(exactText("b"));
         // active todos should be a, c
-        $("li:nth-child(2) a").click();
-        $("li:nth-child(1)").shouldHave(exactText("a"));
-        $("li:nth-child(2)").shouldHave(exactText("c"));
+        $("#todo-list li:nth-child(2) a").click();
+        $("#todo-list li:nth-child(1)").shouldHave(exactText("a"));
+        $("#todo-list li:nth-child(2)").shouldHave(exactText("c"));
     }
 
 }
