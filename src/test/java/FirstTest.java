80faabff3469ffbcd60c47ff1d269fcfe2c2db4e
chamberis <karolisjakstas@gmail.com>
2023-01-13 14:47:58 +0200

    changing nth-child to nth-of-type

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.CollectionCondition;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -28,19 +29,16 @@ public class FirstTest {
         $("#new-todo").setValue("b").pressEnter();
         $("#new-todo").setValue("c").pressEnter();
         // todos should be "a", "b", "c"
-//        $("li:nth-child(1)").shouldHave(exactText("a"));
-//        $("li:nth-child(2)").shouldHave(exactText("b"));
-//        $("li:nth-child(3)").shouldHave(exactText("c"));
-        $$("#todo-list li").shouldHave(CollectionCondition.texts("a", "b", "c"));
+        $$("#todo-list li").shouldHave(texts("a","b","c"));
         // toggle b
-        $("#todo-list li:nth-child(2) .toggle").click();
+        $("#todo-list li:nth-of-type(2) .toggle").click();
         // completed todos should be b
-        $("#footer li:nth-child(3) a").click();
+        $("#footer li:nth-of-type(3) a").click();
         $(".view").shouldHave(exactText("b"));
         // active todos should be a, c
-        $("#footer li:nth-child(2) a").click();
-        $("#todo-list li:nth-child(1)").shouldHave(exactText("a"));
-        $("#todo-list li:nth-child(2)").shouldHave(exactText("c"));
+        $("#footer li:nth-of-type(2) a").click();
+        $("#todo-list li:nth-of-type(1)").shouldHave(exactText("a"));
+        $("#todo-list li:nth-of-type(2)").shouldHave(exactText("c"));
     }
 
 }
