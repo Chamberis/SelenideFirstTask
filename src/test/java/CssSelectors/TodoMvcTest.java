82a39127e7f1d6b73cc3ea57bf880cdf2a9b1b2b
chamberis <karolisjakstas@gmail.com>
2023-01-16 19:22:11 +0200

    packages for Xpath and CssSelectors tests

---
+++ src/test/java/CssSelectors/TodoMvcTest.java
@@ -0,0 +1,23 @@
+package CssSelectors;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Selenide.*;
+public class TodoMvcTest {
+
+    @Test
+    public void crudTodo() {
+        open("https://todomvc.com/examples/emberjs/");
+
+        $("#new-todo").setValue("a").pressEnter();
+        $("#new-todo").setValue("b").pressEnter();
+        $("#new-todo").setValue("c").pressEnter();
+        $$("#todo-list li").shouldHave(exactTexts("a", "b", "c"));
+
+        $("#todo-list li:nth-of-type(2) .toggle").click();
+        $$("#todo-list li.completed").shouldHave(exactTexts("b"));
+        $$("#todo-list li:not(.completed)").shouldHave(exactTexts("a", "c"));
+    }
+
+}
