0db4d84bf3af4346a628070ac265259425ea64f4
chamberis <karolisjakstas@gmail.com>
2023-01-23 19:08:22 +0200

    3. adding static keyword to methods

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -2,15 +2,15 @@ package base;
 
 public class TestBase {
 
-    public String withClass(String className){
+    public static String withClass(String className){
         return  "[contains(concat(' ',normalize-space(@class),' '),' " + className + " ')]";
     }
 
-    public String withoutClass(String className){
+    public static String withoutClass(String className){
         return "[not(contains(concat(' ',normalize-space(@class),' '),' " + className + " '))]";
     }
 
-    public String withText(String text){
+    public static String withText(String text){
         return "[.//text()='" + text + "']";
     }
 }
